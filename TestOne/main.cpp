f2856237adb3b98de0924982aebe0e82466bc396
Vincent Yang <VincentYang@vincents-mbp.gateway.2wire.net>
2014-06-30 19:01:05 -0700

    Initial Commit

---
+++ TestOne/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestOne
+//
+//  Created by Vincent Yang on 6/30/14.
+//  Copyright (c) 2014 VincentYang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
