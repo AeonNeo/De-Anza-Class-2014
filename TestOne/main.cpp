ae701abfa886991b27de7ea6ccd089b1e41b0884
AeonNeo <VincentYang2014@gmail.com>
2014-09-01 12:27:49 -0700

    C++ De Anza

--- TestOne/main.cpp
+++ TestOne/main.cpp
@@ -7,12 +7,60 @@
 //
 
 #include <iostream>
+using namespace std;
+#define thisIsAField "this is a field";
+#define shangway 1.0;
 
 int main(int argc, const char * argv[])
 {
-
+   /** int q; //declaring a variable
+    q=2; //initializing a variable
+    int c = 9/2;
+    double b1 = 9/2;
+    double b = 9.0/2;
+    cout << "the double variable b is " << b << endl;
+    cout << "the double variable b1 is " << b1 << endl;
+    cout << "the it c is " << c << endl;
+    */
+    /*
+    //part one
+    const int l = 5;
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << l << " this cannot be changed since its constant" << endl;
+    bool b = false;
+  //  thisIsAField="this is a field";
+    cout << thisIsAField;
+    //std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n" << endl;
+    cout << "Is this bold" << endl;
+    cout << 5 << " This is a normal integer" << endl;
+    cout << 5u << "This is an unsigned integer" << endl;
+    cout << 5l << "This is a signed long" << endl;
+    cout << "This is a string that takes \
+    two lines";
+    b=!b;
+    cout << b << endl;
+    
+    */
+
+    /*
+    //part two
+    //declaring variables
+    int a, b;
+    //process
+    a=5;
+    b=2;
+    int c=a-b;
+    //print out result
+    cout << c;*/
+    
+    //cout << (7==5 ? 4 : 3); //essentially if/else statement
+    string i;
+    cout << "Please type your name\n";
+    cin >> i;
+    cout << "The value you entered was " << i << endl;
+    
+    //end program
     return 0;
 }
 
